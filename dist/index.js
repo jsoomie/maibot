276ce7b5dc7b83e030649ce714b2c1756b1dd0b3
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-26 17:03:32 -0700

    feat: remove previous files additions of commands and interaces create index with running bot and login

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var discord_js_1 = require("discord.js");
+var client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
 var config_json_1 = __importDefault(require("./config.json"));
-console.log(config_json_1.default.token);
+client.on("ready", function () {
+    console.log("The client is ready");
+});
+client.login(config_json_1.default.token);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 import config from "./config.json";
+
+client.on("ready", () => {
+  console.log("The client is ready");
+});
+
+client.login(config.token);
