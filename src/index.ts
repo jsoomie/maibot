e97ade1e8906ad84673af068ab0cef395a12b899
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-26 17:19:16 -0700

    feat: Add discord rest/builders/types to use with commands Destructure token from config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Client, Intents } from "discord.js";
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-import config from "./config.json";
+import { token } from "./config.json";
 
 client.on("ready", () => {
   console.log("The client is ready");
 });
 
-client.login(config.token);
+client.login(token);
